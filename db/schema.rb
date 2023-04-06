66cb126782f9d725acce88f4268b5b2e050e013a
ONJoseph <on.josephinfo@gmail.com>
2023-04-06 23:44:32 +0100

    Add association between models to schema

--- db/schema.rb
+++ db/schema.rb
@@ -23,12 +23,12 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_06_091518) do
   end
 
   create_table "likes", force: :cascade do |t|
-    t.bigint "user_id", null: false
+    t.bigint "author_id", null: false
     t.bigint "post_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["author_id"], name: "index_likes_on_author_id"
     t.index ["post_id"], name: "index_likes_on_post_id"
-    t.index ["user_id"], name: "index_likes_on_user_id"
   end
 
   create_table "posts", force: :cascade do |t|
@@ -51,5 +51,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_06_091518) do
   end
 
   add_foreign_key "likes", "posts"
-  add_foreign_key "likes", "users"
+  add_foreign_key "likes", "users", column: "author_id"
 end
