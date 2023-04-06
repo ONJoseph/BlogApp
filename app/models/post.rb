ca44df50736eac716928135692c61ee017271241
ONJoseph <on.josephinfo@gmail.com>
2023-04-06 23:48:02 +0100

    Create post model with association and methods

---
+++ app/models/post.rb
@@ -0,0 +1,18 @@
+class Post < ApplicationRecord
+  belongs_to :author, class_name: 'User'
+  has_many :comments
+  has_many :likes
+
+  after_save :update_post_count
+
+  def five_most_recent_comments
+    comments.order(created_at: :desc).first(5)
+  end
+
+  def update_post_count
+    author.increment!(:post_count)
+    update_column(:post_count, author.post_count)
+  end
+
+  attribute :post_count, :integer, default: 0
+end
