89f6bf5fe326bbfe440f1ea79e0d314a3808ceea
ONJoseph <on.josephinfo@gmail.com>
2023-04-07 10:13:22 +0100

    Add validations for post and user

--- app/models/post.rb
+++ app/models/post.rb
@@ -18,3 +18,8 @@ class Post < ApplicationRecord
 
   attribute :post_count, :integer, default: 0
 end
+
+validates :title, presence: true, length: { in: 2..250 }
+  validates :comments_count, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :likes_count, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+end
--- db/schema.rb
+++ db/schema.rb
@@ -15,11 +15,13 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_06_091518) do
   enable_extension "plpgsql"
 
   create_table "comments", force: :cascade do |t|
-    t.integer "user_id"
-    t.integer "post_id"
-    t.text "text"
+    t.bigint "author_id", null: false
+    t.bigint "post_id", null: false
+    t.string "text"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["author_id"], name: "index_comments_on_author_id"
+    t.index ["post_id"], name: "index_comments_on_post_id"
   end
 
   create_table "likes", force: :cascade do |t|
@@ -32,24 +34,28 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_06_091518) do
   end
 
   create_table "posts", force: :cascade do |t|
-    t.integer "author_id"
+    t.bigint "author_id", null: false
     t.string "title"
-    t.text "text"
-    t.integer "comments_counter"
-    t.integer "likes_counter"
+    t.string "text"
+    t.integer "comments_count", default: 0
+    t.integer "likes_count", default: 0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["author_id"], name: "index_posts_on_author_id"
   end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "photo"
-    t.text "bio"
-    t.integer "posts_counter"
+    t.string "bio"
+    t.integer "post_count", default: 0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "comments", "posts"
+  add_foreign_key "comments", "users", column: "author_id"
   add_foreign_key "likes", "posts"
   add_foreign_key "likes", "users", column: "author_id"
+  add_foreign_key "posts", "users", column: "author_id"
 end
