96ee4f447c9b65268463266a2287767d5f80e42f
ONJoseph <on.josephinfo@gmail.com>
2023-04-13 12:29:31 +0100

    Modify and refactor migration files and schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,23 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_11_154432) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_13_092714) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "comments", force: :cascade do |t|
-    t.bigint "author_id", null: false
-    t.bigint "post_id", null: false
+    t.bigint "author_id"
+    t.bigint "post_id"
     t.string "text"
-    t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["author_id"], name: "index_comments_on_author_id"
     t.index ["post_id"], name: "index_comments_on_post_id"
   end
 
   create_table "likes", force: :cascade do |t|
-    t.bigint "author_id", null: false
-    t.bigint "post_id", null: false
+    t.bigint "author_id"
+    t.bigint "post_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["author_id"], name: "index_likes_on_author_id"
@@ -34,7 +33,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_11_154432) do
   end
 
   create_table "posts", force: :cascade do |t|
-    t.bigint "author_id", null: false
+    t.bigint "author_id"
     t.string "title"
     t.string "text"
     t.integer "comments_count", default: 0
@@ -42,6 +41,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_11_154432) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["author_id"], name: "index_posts_on_author_id"
+    t.index ["comments_count"], name: "index_posts_on_comments_count"
+    t.index ["likes_count"], name: "index_posts_on_likes_count"
   end
 
   create_table "users", force: :cascade do |t|
@@ -51,11 +52,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_11_154432) do
     t.integer "post_count", default: 0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["post_count"], name: "index_users_on_post_count"
   end
 
-  add_foreign_key "comments", "posts"
-  add_foreign_key "comments", "users", column: "author_id"
-  add_foreign_key "likes", "posts"
-  add_foreign_key "likes", "users", column: "author_id"
-  add_foreign_key "posts", "users", column: "author_id"
 end
