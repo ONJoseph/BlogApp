2885acc1b1c9729c3fcf1a811363effd7c107e2b
ONJoseph <on.josephinfo@gmail.com>
2023-04-13 12:41:44 +0100

    Update comment and like model with associations

--- app/models/like.rb
+++ app/models/like.rb
@@ -4,9 +4,7 @@ class Like < ApplicationRecord
 
   after_save :update_likes_counter
 
-  private
-
   def update_likes_counter
-    post.increment!(:likes_counter)
+    post.increment!(:likes_count)
   end
 end
