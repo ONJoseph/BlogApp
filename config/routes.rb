c3aadfb5a63013ac64bc58d6ca306a782fe1990c
ONJoseph <on.josephinfo@gmail.com>
2023-04-18 12:54:22 +0100

    Fixed routes bug

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   root to: "users#index"
 
   resources :users, only: [:index, :show] do
-    resources :posts, only: [:index, :show]
+    resources :posts, only: [:index, :show] do
+      post 'like', to: 'post_likes#create'
+    end
   end
-
 end
+
